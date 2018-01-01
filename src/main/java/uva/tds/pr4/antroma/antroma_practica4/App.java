f5f7e5e2f390fc85a2e4ffcd4686fbb9f76c09d2
antroma <antonio.roman@alumnos.uva.es>
2018-01-01 23:53:06 +0100

    First eclipse proyect creation

---
+++ src/main/java/uva/tds/pr4/antroma/antroma_practica4/App.java
@@ -0,0 +1,13 @@
+package uva.tds.pr4.antroma.antroma_practica4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
