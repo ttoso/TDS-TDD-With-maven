605e6b459b4d469f25ae011a35d3c14837c455de
antroma <antonio.roman@alumnos.uva.es>
2018-01-06 06:36:13 +0100

    Added classes Persona and PersonaTest

---
+++ src/test/java/uva/tds/pr4/antroma/PersonaTest.java
@@ -0,0 +1,14 @@
+package uva.tds.pr4.antroma;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PersonaTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
