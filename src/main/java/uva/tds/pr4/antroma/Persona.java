c6caea1614a01ea854e20bb529488d43e42395e7
antroma <antonio.roman@alumnos.uva.es>
2018-01-06 07:10:00 +0100

    Added javadoc for constructor

--- src/main/java/uva/tds/pr4/antroma/Persona.java
+++ src/main/java/uva/tds/pr4/antroma/Persona.java
@@ -1,4 +1,5 @@
 package uva.tds.pr4.antroma;
+
 /**
  * Implementacion simple de una persona con amigos y conocidos
  * 
@@ -7,6 +8,23 @@ package uva.tds.pr4.antroma;
  */
 public class Persona {
 
+	/**
+	 * Constructor de una Persona
+	 * 
+	 * @param nombre
+	 *            Nombre de la persona. Debe ser correcto: No null y no cadena
+	 *            vacia
+	 * @param id
+	 *            Numero identificador de la persona. Debe ser correto: Positivo
+	 * @param amigos
+	 *            Array de los amigos de la persona Debe ser correcto: No nulo
+	 * @param conocidos
+	 *            Array de los conocidos de la persona. Debe ser correcto: No
+	 *            nulo
+	 * @throws IllegalArgumentException
+	 *             En caso de incumplir alguna de las condiciones impuestas a
+	 *             los argumentos del constructor.
+	 */
 	public Persona(String nombre, int id, Persona[] amigos, Persona[] conocidos) {
 		// TODO Auto-generated constructor stub
 	}
