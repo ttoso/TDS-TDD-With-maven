18fb609c8653c685d64cfeff45bd236b4b5717d6
antroma <antonio.roman@alumnos.uva.es>
2018-01-07 01:44:15 +0100

    Added class colaDeAmigos and generated ColaDeAmigosTest

---
+++ src/test/java/uva/tds/pr4/antroma/ColaDeAmigosTest.java
@@ -0,0 +1,14 @@
+package uva.tds.pr4.antroma;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ColaDeAmigosTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
