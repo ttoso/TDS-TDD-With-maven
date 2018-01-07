7b3a3cb4220ca91de81626fdf6955cdbdc378500
antroma <antonio.roman@alumnos.uva.es>
2018-01-07 04:26:55 +0100

    Created suite for all tests

---
+++ src/test/java/uva/tds/pr4/antroma/AllTests.java
@@ -0,0 +1,11 @@
+package uva.tds.pr4.antroma;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ColaDeAmigosTest.class, PersonaTest.class })
+public class AllTests {
+
+}
